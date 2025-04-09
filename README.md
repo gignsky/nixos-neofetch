04779021b841eb62c13b8c8899c758a56c30aa7e
typovrak <typovrak@gmail.com>
2025-04-09 11:30:21 +0200

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# nixos-neofetch
---
+++ configuration.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+let
+	username = "typovrak";
+	group = config.users.users.${username}.group or "users";
+	home = config.users.users.${username}.home;
+in {
+	system.activationScripts.neofetch = ''
+		mkdir -p ${home}/.config
+		chown ${username}:${group} ${home}/.config
+		chmod 700 ${home}/.config
+
+		mkdir ${home}/.config/neofetch
+		chown ${username}:${group} ${home}/.config/neofetch
+		chmod 700 ${home}/.config/neofetch
+
+		cp ${./config.conf} ${home}/.config/neofetch/config.conf
+		chown ${username}:${group} ${home}/.config/neofetch/config.conf
+		chmod 600 ${home}/.config/neofetch/config.conf
+	'';
+
+	environment.systemPackages = with pkgs; [
+		neofetch
+	];
+}
