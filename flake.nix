694ebdeef6be3f6d05c0f880f2708c7c404bd9c4
gignsky <gig@gignsky.com>
2025-05-26 12:31:02 -0400

    updated inputs

--- flake.nix
+++ flake.nix
@@ -2,40 +2,36 @@
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # flake-parts = {
-    #   url = "github:hercules-ci/flake-parts";
-    # };
-    # systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+    systems.url = "github:nix-systems/default";
 
-    # git-hooks = {
-    #   url = "github:cachix/git-hooks.nix";
-    #   flake = false;
-    # };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      flake = false;
+    };
 
-    # treefmt-nix = {
-    #   url = "github:numtide/treefmt-nix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # # personal repos
-    # dotfiles = {
-    #   url = "github:gignsky/dotfiles";
-    #   flake = true;
-    # };
+    # personal repos
+    dotfiles = {
+      url = "github:gignsky/dotfiles";
+      flake = true;
+    };
   };
 
-  # outputs = inputs:
-  #   inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-  #     systems = import inputs.systems;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-  #     # See ./nix/modules/*.nix for the modules that are imported here.
-  #     imports = with builtins;
-  #       map
-  #         (fn: ./nix/modules/${fn})
-  #         (attrNames (readDir ./nix/modules));
-  #   };
-
-  outputs = { self, nixpkgs }: {
-    nixosModules.default = import ./configuration.nix;
-  };
+      # See ./nix/modules/*.nix for the modules that are imported here.
+      imports = with builtins;
+        map
+          (fn: ./nix/modules/${fn})
+          (attrNames (readDir ./nix/modules));
+    };
 }
