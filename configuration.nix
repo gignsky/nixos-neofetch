8ed4bd41046065bc4d78a8bab44cff0a59020e12
gignsky <gig@gignsky.com>
2025-05-26 10:43:17 -0400

    did a little formatting as per just fmt commands output

--- configuration.nix
+++ configuration.nix
@@ -1,26 +1,27 @@
 { config, pkgs, ... }:
 
 let
-	username = config.username;
-	group = config.users.users.${username}.group or "users";
-	home = config.users.users.${username}.home;
-in {
-	system.activationScripts.neofetch = ''
-		mkdir -p ${home}/.config
-		chown ${username}:${group} ${home}/.config
-		chmod 700 ${home}/.config
+  username = config.username;
+  group = config.users.users.${username}.group or "users";
+  home = config.users.users.${username}.home;
+in
+{
+  system.activationScripts.neofetch = ''
+    		mkdir -p ${home}/.config
+    		chown ${username}:${group} ${home}/.config
+    		chmod 700 ${home}/.config
 
-		rm -rf ${home}/.config/neofetch
-		mkdir ${home}/.config/neofetch
-		chown ${username}:${group} ${home}/.config/neofetch
-		chmod 700 ${home}/.config/neofetch
+    		rm -rf ${home}/.config/neofetch
+    		mkdir ${home}/.config/neofetch
+    		chown ${username}:${group} ${home}/.config/neofetch
+    		chmod 700 ${home}/.config/neofetch
 
-		cp ${./config.conf} ${home}/.config/neofetch/config.conf
-		chown ${username}:${group} ${home}/.config/neofetch/config.conf
-		chmod 600 ${home}/.config/neofetch/config.conf
-	'';
+    		cp ${./config.conf} ${home}/.config/neofetch/config.conf
+    		chown ${username}:${group} ${home}/.config/neofetch/config.conf
+    		chmod 600 ${home}/.config/neofetch/config.conf
+    	'';
 
-	environment.systemPackages = with pkgs; [
-		neofetch
-	];
+  environment.systemPackages = with pkgs; [
+    neofetch
+  ];
 }
